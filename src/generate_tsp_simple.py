1b089a0279729c86d0340e510e66e942e7c8b182
Max Vogel <max.vogel2@student.kit.edu>
2025-07-15 17:00:25 +0200

    Add simple version of TSP circuit generation for thesis

---
+++ src/generate_tsp_simple.py
@@ -0,0 +1,24 @@
+import qiskit
+from qiskit import qasm2
+from qiskit.primitives import Sampler
+from qiskit_algorithms import QAOA
+from qiskit_algorithms.optimizers import COBYLA
+from qiskit_optimization.applications import Tsp
+from qiskit_optimization.converters import QuadraticProgramToQubo
+
+
+def generate_tsp_circuit(n: int, seed: int) -> str:
+    instance = Tsp.create_random_instance(n=n, seed=seed)
+
+    qp = instance.to_quadratic_program()
+    qubo = QuadraticProgramToQubo(penalty=None).convert(qp)
+    operator, _ = qubo.to_ising()
+    qaoa = QAOA(
+        sampler=Sampler(),
+        optimizer=COBYLA(maxiter=500),
+        reps=3
+    )
+    optimal_params = qaoa.compute_minimum_eigenvalue(operator).optimal_point
+    circuit = qaoa.ansatz.assign_parameters(optimal_params)
+
+    return qiskit.qasm2.dumps(circuit)
